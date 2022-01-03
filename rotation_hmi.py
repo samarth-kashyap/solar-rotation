333a9384b17ee30801de29bff882548b49a8ceb6
Samarth G Kashyap <g.samarth@tifr.res.in>
2022-01-03 14:32:16 +0530

    interpolation of 2d profile uncertainities

--- rotation_hmi.py
+++ rotation_hmi.py
@@ -25,23 +25,29 @@ def pLegSave(l, x, filePrefix, overwrite=False):
         LP0 = LP1;
         LP1 = LP2;
 
+def get_both_hemispheres(w2d_upper):
+    w2d_lower = w2d_upper[:, ::-1][:, 1:]
+    w2d = np.zeros((lenr, w2d_upper.shape[1]*2-1))
+
+    lent = w2d_upper.shape[1]
+    w2d[:, :lent] = w2d_upper
+    w2d[:, lent:] = w2d_lower
+    return w2d
+
+
 if __name__=="__main__":
     rmesh = np.loadtxt('input_files/rmesh.hmi')[::4]
     lenr = len(rmesh)
 
-    rot2d_upper = np.loadtxt('input_files/rot2d.hmi')
-    rot2d_lower = rot2d_upper[:, ::-1][:, 1:]
-    rot2d = np.zeros((lenr, rot2d_upper.shape[1]*2-1))
-
-    lent = rot2d_upper.shape[1]
-    rot2d[:, :lent] = rot2d_upper
-    rot2d[:, lent:] = rot2d_lower
-
-    tmesh = np.arange(rot2d_upper.shape[1])
+    rot2d = np.loadtxt('input_files/rot2d.hmi')
+    err2d = np.loadtxt('input_files/err2d.hmi')
+    tmesh = np.arange(rot2d.shape[1])
     theta = 90 - tmesh*15./8
-    # theta = np.append(theta[::-1][:-1], 180. - theta)
     theta = np.append(-theta, theta[::-1][1:]) + 90.
     theta = theta*pi/180.
+
+    rot2d = get_both_hemispheres(rot2d)
+    err2d = get_both_hemispheres(err2d)
 
     costh = np.cos(theta)
     sinth = np.sin(theta)
@@ -56,9 +62,24 @@ if __name__=="__main__":
     omega8 = np.zeros(lenr)
     omega10 = np.zeros(lenr)
 
+    err0 = np.zeros(lenr)
+    err2 = np.zeros(lenr)
+    err4 = np.zeros(lenr)
+    err6 = np.zeros(lenr)
+    err8 = np.zeros(lenr)
+    err10 = np.zeros(lenr)
+
+
+
     w1 = np.zeros(lenr)
     w3 = np.zeros(lenr)
     w5 = np.zeros(lenr)
+
+    ord_list = np.arange(0, 12, 2)
+    lp_list = []
+    for ord in ord_list:
+        _lp = fits.open(f'data_files/lp_n{ord:02d}.fits')[0].data
+        lp_list.append(_lp)
 
     lp0 = fits.open('data_files/lp_n00.fits')[0].data
     lp2 = fits.open('data_files/lp_n02.fits')[0].data
@@ -74,15 +95,30 @@ if __name__=="__main__":
         omega6[i] = simps(rot2d[i, :]*lp6, costh)*(2*6+1.)/2.
         omega8[i] = simps(rot2d[i, :]*lp8, costh)*(2*8+1.)/2.
         omega10[i] = simps(rot2d[i, :]*lp10, costh)*(2*10+1.)/2.
+        
+        err0[i] = simps(err2d[i, :]*lp0, costh)*(2*0+1.)/2.
+        err2[i] = simps(err2d[i, :]*lp2, costh)*(2*2+1.)/2.
+        err4[i] = simps(err2d[i, :]*lp4, costh)*(2*4+1.)/2.
+        err6[i] = simps(err2d[i, :]*lp6, costh)*(2*6+1.)/2.
+        err8[i] = simps(err2d[i, :]*lp8, costh)*(2*8+1.)/2.
+        err10[i] = simps(err2d[i, :]*lp10, costh)*(2*10+1.)/2.
     
     w1 = 2*sqrt(pi/3.)*rmesh*(omega0/1. - omega2/5.)
     w3 = 2*sqrt(pi/7.)*rmesh*(omega2/5. - omega4/9.)
     w5 = 2*sqrt(pi/11)*rmesh*(omega4/9.)
 
+    e1 = 2*sqrt(pi/3.)*rmesh*(err0/1. - err2/5.)
+    e3 = 2*sqrt(pi/7.)*rmesh*(err2/5. - err4/9.)
+    e5 = 2*sqrt(pi/11)*rmesh*(err4/9.)
+
     writefitsfile(w1, 'output_files/w1rot-hmi.fits', overwrite=True)
     writefitsfile(w3, 'output_files/w3rot-hmi.fits', overwrite=True)
     writefitsfile(w5, 'output_files/w5rot-hmi.fits', overwrite=True)
+    writefitsfile(e1, 'output_files/w1err-hmi.fits', overwrite=True)
+    writefitsfile(e3, 'output_files/w3err-hmi.fits', overwrite=True)
+    writefitsfile(e5, 'output_files/w5err-hmi.fits', overwrite=True)
     writefitsfile(rmesh, 'output_files/rad-hmi.fits', overwrite=True)
 
     plt.plot(rmesh, w1)
+    plt.fill_between(rmesh, w1-e1, w1+e1, alpha=0.5)
     plt.show()
--- w35_interp_hmi.py
+++ w35_interp_hmi.py
@@ -13,11 +13,21 @@ def writefitsfile(a, fname):
     hdu.writeto(fname, overwrite=True)
     return None
 
+
+def fix_extrapolation(wk, interp_func):
+    wk[rmin_idx:rmax_idx] = interp_func(r[rmin_idx:rmax_idx])
+    wk[rmax_idx:] = wk[rmax_idx-1]
+    wk[:rmin_idx] = wk[rmin_idx]
+    return wk
+
 # r = np.squeeze(fits.open('radius.fits')[0].data)
 r = np.loadtxt('input_files/r_jesper.dat')
 w1mesh = np.squeeze(fits.open('output_files/w1rot-hmi.fits')[0].data)
 w3mesh = np.squeeze(fits.open('output_files/w3rot-hmi.fits')[0].data)
 w5mesh = np.squeeze(fits.open('output_files/w5rot-hmi.fits')[0].data)
+e1mesh = np.squeeze(fits.open('output_files/w1err-hmi.fits')[0].data)
+e3mesh = np.squeeze(fits.open('output_files/w3err-hmi.fits')[0].data)
+e5mesh = np.squeeze(fits.open('output_files/w5err-hmi.fits')[0].data)
 rmesh = np.squeeze(fits.open('output_files/rad-hmi.fits')[0].data)
 
 
@@ -34,22 +44,28 @@ except IndexError:
 wint1 = interp.interp1d(rmesh, w1mesh)
 wint3 = interp.interp1d(rmesh, w3mesh)
 wint5 = interp.interp1d(rmesh, w5mesh)
+eint1 = interp.interp1d(rmesh, e1mesh)
+eint3 = interp.interp1d(rmesh, e3mesh)
+eint5 = interp.interp1d(rmesh, e5mesh)
+
 w1 = np.zeros(r.shape[0])
 w3 = np.zeros(r.shape[0])
 w5 = np.zeros(r.shape[0])
+e1 = np.zeros(r.shape[0])
+e3 = np.zeros(r.shape[0])
+e5 = np.zeros(r.shape[0])
 
-w1[rmin_idx:rmax_idx] = wint1(r[rmin_idx:rmax_idx])
-w1[rmax_idx:] = w1[rmax_idx-1]
-w1[:rmin_idx] = w1[rmin_idx]
-
-w3[rmin_idx:rmax_idx] = wint3(r[rmin_idx:rmax_idx])
-w3[rmax_idx:] = w3[rmax_idx-1]
-w3[:rmin_idx] = w3[rmin_idx]
-
-w5[rmin_idx:rmax_idx] = wint5(r[rmin_idx:rmax_idx])
-w5[rmax_idx:] = w5[rmax_idx-1]
-w5[:rmin_idx] = w5[rmin_idx]
+w1 = fix_extrapolation(w1, wint1)
+w3 = fix_extrapolation(w3, wint3)
+w5 = fix_extrapolation(w5, wint5)
+e1 = fix_extrapolation(e1, eint1)
+e3 = fix_extrapolation(e3, eint3)
+e5 = fix_extrapolation(e5, eint5)
 
 writefitsfile(w1, 'output_files/w1_interp-hmi.fits')
 writefitsfile(w3, 'output_files/w3_interp-hmi.fits')
 writefitsfile(w5, 'output_files/w5_interp-hmi.fits')
+
+writefitsfile(e1, 'output_files/e1_interp-hmi.fits')
+writefitsfile(e3, 'output_files/e3_interp-hmi.fits')
+writefitsfile(e5, 'output_files/e5_interp-hmi.fits')
